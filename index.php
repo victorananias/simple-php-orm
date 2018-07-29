1723e3da741eb8367b68e0a8d9baf858a16a0e56
Victor <victor.ananias8@gmail.com>
2018-07-29 12:51:03 -0300

    DONE

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html>
-	<head>
-	<style>
-		header {
-			background-color: #e3e3e3;
-			padding: 2em;
-			text-align: center
-		}
-	</style>
-	</head>
-	<body>
-		<header>
-			<h1>
-				<?= "Hello, ".htmlspecialchars($_GET['nome']) ?>
-			</h1>
-		</header>
-	</body>
-</html>
+<?php
+
+
+$cumprimento = "Hello, ".htmlspecialchars($_GET['nome']);
+
+require "index.view.php";
---
+++ index.view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<style>
+		header {
+			background-color: #e3e3e3;
+			padding: 2em;
+			text-align: center
+		}
+	</style>
+	</head>
+	<body>
+		<header>
+			<h1>
+				<?= $cumprimento ?>
+			</h1>
+		</header>
+	</body>
+</html>
