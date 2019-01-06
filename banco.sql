2148beb1f9871f20d3add48e2bc8820d3ea9deb2
victor <victor.ananias8@mgail.com>
2019-01-06 12:07:38 -0200

    config alterada

--- banco.sql
+++ banco.sql
@@ -1,21 +1,22 @@
+DROP DATABASE IF EXISTS banco_teste;
+
 CREATE DATABASE banco_teste;
+
 USE banco_teste;
 
 CREATE TABLE TblProduto(
-    ProdutoID int not null auto_increment, 
-    NmProduto varchar(250),
-    DtCadastro datetime default CURRENT_TIMESTAMP,
-    SegmentoID  int,
-    primary key(ProdutoID)
+    ProdutoID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+    NmProduto VARCHAR(250),
+    DtCadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
+    SegmentoID  INT
 );
 
 CREATE TABLE TblSegmento(
-    SegmentoID int not null auto_increment,
-    NmSegmento varchar(250),
-    Descricao text,
-    primary key(SegmentoID)
+    SegmentoID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    NmSegmento VARCHAR(250),
+    Descricao TEXT
 );
 
-INSERT INTO TblSegmento(NmSegmento, Descricao) VALUES( "Saúde", "Produtos utilizado para Saúde. "), 
-( "Cosméticos", "Produtos utilizado para Cosméticos. "), 
-( "Alimentício", "Produtos utilizado para Alimentício. ");
+INSERT INTO TblSegmento VALUES(NULL, "Saúde", "Produtos utilizado para Saúde."), 
+(NULL, "Cosméticos", "Produtos utilizado para Cosméticos."), 
+(NULL, "Alimentício", "Produtos utilizado para Alimentício.");
---
+++ config.example.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'database' => [
+        'connection' => "mysql:host=127.0.0.1",
+        'dbname' => "banco_teste",
+        'charset' => "UTF8",
+        'username' => "victor",
+        'password' => "victor",
+        'config' => []
+    ]
+];
